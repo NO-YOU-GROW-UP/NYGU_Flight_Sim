84a5a4865ed702876919b7fef27e92db0c9e2cc0
Mickey <49824883+noyougrowup@users.noreply.github.com>
2021-01-30 13:55:30 -0500

    Plane Movement Component Created
    Plane Movement Component Created

---
+++ Source/NYGU_Flight_Sim/PlaneMovementComponent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlaneMovementComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NYGU_FLIGHT_SIM_API UPlaneMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPlaneMovementComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
